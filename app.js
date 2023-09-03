7985df635d4ca1c138673e9b182a4986e06d36a3
edilson gomes de lima <edskateicevida@gmail.com>
2023-09-02 22:55:10 -0300

    Update program

---
+++ app.js
@@ -0,0 +1,18 @@
+alert("This is javaScript with alert");
+
+console.log("Olá, mundo!");
+function soma() {
+    console.log(4 + 6)
+}
+soma()
+
+
+let cores = ['amarelo', 'vermelho', 'azul', 'preto' ]
+console.log(cores);
+cores.push('verde')
+        console.log(cores) //saída:  ['amarelo', 'vermelho', 'azul', 'preto', 'verde']
+
+        cores.pop()
+        console.log(cores) //saída:  ['amarelo', 'vermelho', 'azul', 'preto']
+
+        console.log(cores.length) //saída:  4
